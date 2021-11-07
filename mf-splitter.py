d3d6eaa1b3dc7cd18270ce2b1dfe3ad187c90d93
dn503 <dn503@dn503>
2021-11-07 14:07:55 +0000

    Added cmdline options

--- mf-splitter.py
+++ mf-splitter.py
@@ -1,1 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Unofficial Minifantasy sprite splitter and organizer by dn503 (Python is not my main language - so be kind)
+# Apache License 2.0 - see accompanying file
+
+import sys, getopt
+
+def usage():
+    print('splitter.py -i <inputdir>')
+
+def main(argv):
+    inputdir = ''
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:], 'hi:',
+                ['inputdir='])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    for (opt, arg) in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ('-i', '--inputdir'):
+            inputdir = arg
+    if inputdir == '':
+        usage()
+        sys.exit(2)
+
+if __name__ == '__main__':
+    main(sys.argv)
 
