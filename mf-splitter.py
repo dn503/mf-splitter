2e457182c588473580f2faa5ee40dc58ea856bfc
dn503 <dn503@dn503>
2021-11-20 15:18:05 +0000

    Added recursive file/dir list function

--- mf-splitter.py
+++ mf-splitter.py
@@ -4,30 +4,52 @@
 # Unofficial Minifantasy sprite splitter and organizer by dn503 (Python is not my main language - so be kind)
 # Apache License 2.0 - see accompanying file
 
-import sys, getopt
+from pathlib import Path
+import sys, getopt, os
 
-def usage():
+def getfilesrecursive(dir, ext):
+    files = []
+    dot_ext = "." + ext.lower()
+    for it in os.scandir(dir):
+        if it.is_file:
+            if it.path.lower().endswith(dot_ext):
+                files.append(it.path)
+            elif it.is_dir():
+                files += getfilesrecursive(it.path, ext)
+    return files
+
+def print_usage():
     print('splitter.py -i <inputdir>')
 
 def main(argv):
-    inputdir = ''
+    input_path = ''
     try:
         (opts, args) = getopt.getopt(sys.argv[1:], 'hi:',
-                ['inputdir='])
+                ['input_path='])
     except getopt.GetoptError as err:
         print(err)
-        usage()
+        print_usage()
         sys.exit(2)
     for (opt, arg) in opts:
         if opt == '-h':
-            usage()
+            print_usage()
             sys.exit()
-        elif opt in ('-i', '--inputdir'):
-            inputdir = arg
-    if inputdir == '':
-        usage()
+        elif opt in ('-i', '--input_path'):
+            input_path = arg
+    if input_path == '':
+        print_usage()
         sys.exit(2)
+        
+    if not os.path.exists(input_path):
+        print("supplied input_path (" + input_path + ") does not exist!")
+        sys.exit(2)
+        
+    if not os.path.isdir(input_path):
+        print("supplied input_path (" + input_path + ") is not a directory!")
+        sys.exit(2)
+
+    files = getfilesrecursive(input_path, "png")
+    print(files)
 
 if __name__ == '__main__':
     main(sys.argv)
-
